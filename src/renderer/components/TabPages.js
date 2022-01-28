b12a4bc8a87469974f0ed0f97d17583fbeb150ef
Kyusoo <kyusoo@kakao.com>
2022-01-29 08:51:52 +0900

    [SSF] V1.0.1
    [Desc] Fix BUG
     @ Critical
      - 'views' is not included when packaging build
     @ Major
      - IP Address array is not parsed properly

--- src/renderer/components/TabPages.js
+++ src/renderer/components/TabPages.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { Box, Button, Checkbox, Divider, FormControlLabel, Grid, Input, List, ListItem, ListItemButton, ListItemText, Switch, Tooltip, Typography } from '@mui/material'
 import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
-import { Dialog, DialogActions, DialogContent, DialogTitle} from '@mui/material'
+import { Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material'
 import { makeStyles } from '@mui/styles'
 import IPC from '../../ipc'
 import Util from '../../util'
@@ -16,16 +16,17 @@ const PageServer = () => {
         dispatch({ type: 'setIsServerOn', isServerOn: checked })
     }
 
-    const onServerPassword = ({target:{value}}) => {
+    const onServerPassword = ({ target: { value } }) => {
         dispatch({ type: 'setServerPassword', serverPassword: value })
     }
 
     const onSelectIpAddress = ({ target }) => {
-       dispatch({ type: 'snackbar', req: 'copyUrl', snackbar: {
+        dispatch({
+            type: 'snackbar', req: 'copyUrl', snackbar: {
                 open: true,
                 severity: 'info',
                 message: 'Copied to clipboard.',
-                anchorOrigin : {
+                anchorOrigin: {
                     vertical: 'bottom', horizontal: 'right'
                 },
                 data: target.textContent
@@ -48,7 +49,7 @@ const PageServer = () => {
                     <List>
                         {
                             (state.ipAddresses.length > 0) ?
-                                [state.ipAddresses].map((address, i) => {
+                                state.ipAddresses.map((address, i) => {
                                     return (
                                         <React.Fragment key={i}>
                                             <ListItem disablePadding>
@@ -71,13 +72,13 @@ const PageServer = () => {
 
 const useStyles = makeStyles({
     labelFileSelect: {
-        border:'1px solid skyblue',
-        color:'skyblue',
-        borderRadius:'4px',
-        padding:'4px 20px',
-        cursor:'pointer',
+        border: '1px solid skyblue',
+        color: 'skyblue',
+        borderRadius: '4px',
+        padding: '4px 20px',
+        cursor: 'pointer',
         '&:hover': {
-            border:'1px solid #1976D2',
+            border: '1px solid #1976D2',
             color: '#1976D2',
             backgroundColor: '#F6FAFD'
         }
@@ -105,7 +106,7 @@ const PageUpload = () => {
 
         console.log(`[GetSecure] value=${value} url=${url}`)
 
-        if(url.length > 0) {
+        if (url.length > 0) {
             const xhr = new XMLHttpRequest()
             xhr.open('POST', `${url}/secure`)
             xhr.timeout = 200
@@ -128,7 +129,7 @@ const PageUpload = () => {
 
         const password = value ? value : state.upload.password
 
-        const data = {password}
+        const data = { password }
         const xhr = new XMLHttpRequest()
 
         xhr.timeout = 250
@@ -147,20 +148,20 @@ const PageUpload = () => {
         xhr.send(JSON.stringify(data))
     }
 
-    const urlChangeHandler = ({target:{value}}) => {
-        dispatch({type:'upload', upload:{...state.upload, url:value}})
+    const urlChangeHandler = ({ target: { value } }) => {
+        dispatch({ type: 'upload', upload: { ...state.upload, url: value } })
         const isValid = Util.isValidUrl(value)
-        if(isValid) {
+        if (isValid) {
             getIsSecure(value)
         }
     }
 
-    const passwordChangeHandler = ({target:{value}}) => {
-        dispatch({type:'upload', upload:{...state.upload, password:value}})
+    const passwordChangeHandler = ({ target: { value } }) => {
+        dispatch({ type: 'upload', upload: { ...state.upload, password: value } })
         getAuth(value)
     }
 
-    const inputFileHandler = ({target:{files}}) => {
+    const inputFileHandler = ({ target: { files } }) => {
 
         console.dir(files)
 
@@ -179,17 +180,17 @@ const PageUpload = () => {
 
         xhrUpload.upload.onload = e => {
             console.log('upload complete', e);
-            dispatch({type:'upload', upload:{...state.upload, active:false}})
+            dispatch({ type: 'upload', upload: { ...state.upload, active: false } })
         }
         xhrUpload.upload.onloadstart = e => {
             console.log('upload start')
             setProgress(0)
-            dispatch({type:'upload', upload:{...state.upload, active:true}})
-            setDialog({...dialog, open:true})
+            dispatch({ type: 'upload', upload: { ...state.upload, active: true } })
+            setDialog({ ...dialog, open: true })
         }
 
         xhrUpload.upload.onprogress = e => {
-            if( e.lengthComputable ) {
+            if (e.lengthComputable) {
                 const percentComplete = Math.floor(e.loaded / e.total * 100);
                 setProgress(percentComplete)
                 console.log(percentComplete)
@@ -199,15 +200,17 @@ const PageUpload = () => {
         }
         xhrUpload.upload.onabort = () => {
             setProgress(0)
-            dispatch({type:'upload', upload:{...state.upload, active:false}})
-            dispatch({type: 'snackbar', snackbar: {
-                open: true,
-                severity: 'error',
-                message: 'Upload cancelled',
-                anchorOrigin : {
-                    vertical: 'bottom', horizontal: 'right'
+            dispatch({ type: 'upload', upload: { ...state.upload, active: false } })
+            dispatch({
+                type: 'snackbar', snackbar: {
+                    open: true,
+                    severity: 'error',
+                    message: 'Upload cancelled',
+                    anchorOrigin: {
+                        vertical: 'bottom', horizontal: 'right'
+                    }
                 }
-            }})
+            })
         }
 
         xhrUpload.onerror = error => {
@@ -218,11 +221,12 @@ const PageUpload = () => {
             const data = JSON.parse(e.target.response)
             if (data?.result === 'OK') {
 
-                dispatch({ type: 'snackbar', snackbar: {
+                dispatch({
+                    type: 'snackbar', snackbar: {
                         open: true,
                         severity: 'success',
                         message: data.message,
-                        anchorOrigin : {
+                        anchorOrigin: {
                             vertical: 'bottom', horizontal: 'right'
                         },
                         data: null
@@ -243,13 +247,13 @@ const PageUpload = () => {
     }
 
     const uploadCancelHandler = e => {
-        if(window.xhrUpload) {
+        if (window.xhrUpload) {
             window.xhrUpload.abort()
             window.xhrUpload = null
         }
 
         setUploadFiles([])
-        setDialog({...dialog, open:false})
+        setDialog({ ...dialog, open: false })
     }
 
     useEffect(() => {
@@ -267,61 +271,61 @@ const PageUpload = () => {
                     <Grid xs={4} item>URL</Grid>
                     <Grid xs={8} item><Input placeholder="http://address:port" onChange={urlChangeHandler} fullWidth={true} value={state.upload.url} /></Grid>
                 </Grid>
-                { isValidUrl && isSecure &&
-                <Grid container spacing={1} direction="row" alignItems="center" justifyContent="space-between">
-                    <Grid xs={4} item>Upload Password</Grid>
-                    <Grid xs={8} item><Input type="password" placeholder="Upload Password" error={!isAuth} fullWidth={true} onChange={passwordChangeHandler} value={state.upload.password} /></Grid>
-                </Grid>
-                }
-                { ( ( isValidUrl && !isSecure ) || ( isValidUrl && isSecure && isAuth )) &&
-                <Grid container spacing={1} direction="row" alignItems="center" justifyContent="space-between" sx={{marginTop:'10px'}}>
-                    <Grid item>
-                        <form id="formFile" method="POST" encType="multipart/form-data">
-                            <label className={classes.labelFileSelect} htmlFor="inputFile">File Select</label>
-                            <input id="inputFile" type="file" name="upload" multiple onChange={inputFileHandler} style={{display:"none"}} disabled={state.upload.active} />
-                        </form>
+                {isValidUrl && isSecure &&
+                    <Grid container spacing={1} direction="row" alignItems="center" justifyContent="space-between">
+                        <Grid xs={4} item>Upload Password</Grid>
+                        <Grid xs={8} item><Input type="password" placeholder="Upload Password" error={!isAuth} fullWidth={true} onChange={passwordChangeHandler} value={state.upload.password} /></Grid>
                     </Grid>
-                    <Grid item><Button disabled={!isValidUrl || uploadFiles.length === 0} variant="outlined" size="small" onClick={uploadHandler}>Upload</Button></Grid>
-                </Grid>
+                }
+                {((isValidUrl && !isSecure) || (isValidUrl && isSecure && isAuth)) &&
+                    <Grid container spacing={1} direction="row" alignItems="center" justifyContent="space-between" sx={{ marginTop: '10px' }}>
+                        <Grid item>
+                            <form id="formFile" method="POST" encType="multipart/form-data">
+                                <label className={classes.labelFileSelect} htmlFor="inputFile">File Select</label>
+                                <input id="inputFile" type="file" name="upload" multiple onChange={inputFileHandler} style={{ display: "none" }} disabled={state.upload.active} />
+                            </form>
+                        </Grid>
+                        <Grid item><Button disabled={!isValidUrl || uploadFiles.length === 0} variant="outlined" size="small" onClick={uploadHandler}>Upload</Button></Grid>
+                    </Grid>
                 }
             </Grid>
 
             {uploadFiles.length > 0 &&
-            <Box>
-                <Divider sx={{margin:'10px 0px'}} />
-                <TableContainer component={Paper}>
-                    <Table sx={{minWidth: 400}} size="small">
-                        <TableHead sx={{backgroundColor:'#EEE'}}>
-                            <TableRow>
-                                <TableCell sx={{fontWeight:'bold'}} align="left">File</TableCell>
-                                <TableCell sx={{fontWeight:'bold'}} align="right">Size</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                                {uploadFiles.map( (file, idx) => (
+                <Box>
+                    <Divider sx={{ margin: '10px 0px' }} />
+                    <TableContainer component={Paper}>
+                        <Table sx={{ minWidth: 400 }} size="small">
+                            <TableHead sx={{ backgroundColor: '#EEE' }}>
+                                <TableRow>
+                                    <TableCell sx={{ fontWeight: 'bold' }} align="left">File</TableCell>
+                                    <TableCell sx={{ fontWeight: 'bold' }} align="right">Size</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {uploadFiles.map((file, idx) => (
                                     <TableRow key={`upload-file-${idx}`}>
                                         <TableCell>{file.name}</TableCell>
                                         <TableCell align="right">{`${file.sizeInfo.number} ${file.sizeInfo.unit}`}</TableCell>
                                     </TableRow>
                                 ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            </Box>
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Box>
             }
 
-            { state.upload.active &&
-            <Dialog open={dialog.open}>
-                <DialogTitle>{"File Upload"}</DialogTitle>
-                <DialogContent>
-                    <Box sx={{minWidth: 400}}>
-                        <LinearProgressWithValue value={progress} />
-                    </Box>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={uploadCancelHandler} disabled={!state.upload.active} size="small" variant="outlined">Cancel</Button>
-                </DialogActions>
-            </Dialog>
+            {state.upload.active &&
+                <Dialog open={dialog.open}>
+                    <DialogTitle>{"File Upload"}</DialogTitle>
+                    <DialogContent>
+                        <Box sx={{ minWidth: 400 }}>
+                            <LinearProgressWithValue value={progress} />
+                        </Box>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={uploadCancelHandler} disabled={!state.upload.active} size="small" variant="outlined">Cancel</Button>
+                    </DialogActions>
+                </Dialog>
             }
         </div>
     )
